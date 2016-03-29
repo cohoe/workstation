2c4441aa7edaa6b8a00ae0ed203f1d6f77c9b21b
Grant Cohoe <grant.cohoe@rsa.com>
2016-03-29 13:31:22 -0400

    added puppet config

---
+++ dev.pp
@@ -0,0 +1,88 @@
+class systools {
+	# System Tools
+	package { 'iftop': }
+	package { 'glances': }
+	package { 'setroubleshoot-server': }
+}
+
+class devtools {
+	# Shells
+	package { 'zsh': }
+
+	# Editors and workflow
+	package { 'vim-enhanced': }
+	package { 'tmux': }
+	package { 'screen': }
+	package { 'strace': }
+	package { 'git': }
+
+	# Java
+	package { 'java': }
+	package { 'icedtea-web': }
+
+	# Python
+	package { 'python-virtualenvwrapper': }
+	package { 'python-pep8': }
+
+	# LaTeX
+	package { 'texworks': }
+	package { 'texlive-pdftex': }
+
+	# Goups
+	yumgroup { 'Development Tools':
+		ensure => 'present',
+	}
+}
+
+class nettools {
+	# Packet Capture
+	package { 'nmap': }
+	package { 'tcpdump': }
+	package { 'wireshark-gnome': }
+	package { 'net-snmp-utils': }
+
+	# LDAP
+	package { 'openldap-clients': }
+}
+
+class browsers {
+	# Chrome
+	include '::chromerepo'
+	package { 'google-chrome-stable': }
+
+	# Firefox
+	package { 'firefox': }
+
+	# @TODO: Flash
+}
+
+class gui {
+	# Basic tools
+	package { 'launchy': }
+	package { 'numlockx': }
+}
+
+node default {
+	# Passwordless sudo for admin group
+	class { 'sudo': }
+	sudo::conf { 'wheel':
+		priority => 10,
+		content	 => "%wheel ALL=(ALL) NOPASSWD: ALL",
+	}
+
+	# @TODO: RPMFusion
+
+	# Tools
+	include systools
+	include devtools
+	include nettools
+	include browsers
+	include gui
+
+	# SSH
+	service { 'sshd':
+		ensure => 'running',
+		enable => true,
+		hasrestart => true,
+	}
+}
