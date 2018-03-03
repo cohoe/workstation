40cea424c6164aae1e214855dcc1cb84341cc1c4
Grant Cohoe <grant@grantcohoe.com>
2018-03-03 15:04:59 -0500

    removed the docker container because I never use it anymore

--- container/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ANSIBLE_REPO_SRC_PATH=~/Projects/priv/workstation
-ANSIBLE_REPO_DEST_PATH=/workstation
-
-sudo docker run -v "${ANSIBLE_REPO_SRC_PATH}":"${ANSIBLE_REPO_DEST_PATH}" -it "workstation":"testing" /bin/bash
