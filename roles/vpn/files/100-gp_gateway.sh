146318da0ef17f2bc911a8d0a367db2322a26938
Grant Cohoe <grant@grantcohoe.com>
2020-01-14 16:09:41 -0500

    gateway

--- roles/vpn/files/100-gp_gateway.sh
+++ roles/vpn/files/100-gp_gateway.sh
@@ -5,7 +5,9 @@
 # For some reason the GlobalProtect VPN doesn't add a default route anymore.
 # Maybe it didn't in the first place? Well it does it when calling openconnect
 # manually, but not via NetworkManager
+# Also delete any SSH ControlMasters that have been established.
 
 if [[ "$1" = "vpn0" && "$2" = "vpn-up" ]]; then
   ip route add default dev ${1}
+  rm -rf /tmp/ssh-*
 fi
